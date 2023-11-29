bcfa16e85dde2e81bec864dff121b44d417454a0
Ömer Geysoğlu <ogeysoglu@outlook.com>
2023-11-29 14:47:43 +0300

    initial commit

--- streamlit_server.py
+++ streamlit_server.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 from predict_img import BeerDetector
+import numpy as np
+import cv2
 
 
 st.title('Fill or Empty')
@@ -18,11 +20,14 @@ if uploaded_file is not None:
     if 'image' in uploaded_file.type:
         try:
             image = Image.open(uploaded_file)
+            np_image = np.array(image)
+            cv_image = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
             with col1: 
                 st.image(image, caption='The Uploaded Photograph')
             
             if button:
-                detected_image = detectorI(image)
+                detected_image = detectorI.predict(cv_image)
+                detected_image = detectorI.draw_boxes(cv_image, detected_image)
                 with col2:
                     st.image(detected_image, caption="The Detected Photograph", use_column_width="True")
         except Exception as e:
